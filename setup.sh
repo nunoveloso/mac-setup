bfa7f1a664b427275fed3e6cbfbee41d7e677614
Nuno Veloso <nunoveloso18@gmail.com>
2017-11-19 16:37:08 +0000

    Git!

--- setup.sh
+++ setup.sh
@@ -2,3 +2,9 @@
 
 # Homebew 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Git
+brew install git
+cd ~
+curl -O https://raw.githubusercontent.com/nicolashery/mac-dev-setup/master/.gitconfig
+git config --global user.name "Nuno Veloso"
+git config --global user.email "nuno@marzeelabs.org"
